f6e77dcdcd1acf7786cf2345f3bdb4be6612c053
beovan <truongvangiap04@gmail.com>
2024-07-16 15:24:57 +0700

    conditional and object

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,24 @@
 
 <script setup>
 import { ref, reactive } from "vue";
-const isLoggedIn = ref(true);
-const toggleLogin = () => {
-  isLoggedIn.value = !isLoggedIn.value;
-};
-const isVisible = ref(true);
-const toggleShow = () => {
-  isVisible.value = !isVisible.value;
-};
-const userStatus = ref("online");
-const changeStatus = () => {
-  if (userStatus.value === "online") {
-    userStatus.value = "away";
-  } else if (userStatus.value === "away") {
-    userStatus.value = "busy";
-  } else if (userStatus.value === "busy") {
-    userStatus.value = "online";
-  }
-};
+const users = ref([
+  { name: "john", age: 30 , isActive: true },
+  { name: "cho", age: 30 , isActive: false },
+  { name: "meo", age: 30 , isActive: true },
+
+]);
 </script>
 
 <template>
   <div>
-    <h1 v-if="isLoggedIn">Chào mừng người dùng đã đăng nhập</h1>
-    <h1 v-else>Chào mừng khách</h1>
-    <button @click="toggleLogin">{{ isLoggedIn ? "Đăng xuất" : "Đăng nhập" }}</button>
-    <br />
-    <h1 v-if="userStatus === 'online'">Người dùng đang online</h1>
-    <h1 v-else-if="userStatus === 'away'">Người dùng đang vắng mặt</h1>
-    <h1 v-else-if="userStatus === 'busy'">Người dùng đang bận</h1>
-    <h1 v-else>Không rõ trạng thái</h1>
-    <button @click="changeStatus">Thay đổi trạng thái</button>
-    <br />
-    <h1 v-show="isVisible">Nội dung này ẩn hoặc hiển thị</h1>
-    <button @click="toggleShow">{{ isVisible ? "Ẩn" : "Hiện" }}</button>
-  </div>
+   <div>
+    <ul>
+      <template v-for="(user, index) in users" :key="user.name">
+        <li v-if="user.isActive" :key="user.name">
+          {{ user.name }}:  {{ user.isActive ? "Active" : "Deactive" }}({{ index }})
+        </li>
+      </template>
+    </ul>
+   </div>
+</div>
 </template>
